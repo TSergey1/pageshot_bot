0f24d48d729334c29c7a85af7858dcb7a4ee026d
Сергей Тыртычный <tyrtychnyy90@mail.ru>
2024-04-20 22:18:40 +0300

    12

--- app/handlers/menu.py
+++ app/handlers/menu.py
@@ -1,5 +1,3 @@
-import asyncio
-import time
 from aiogram import F, Router
 from aiogram.filters import CommandStart
 # from aiogram.fsm.context import FSMContext
@@ -64,67 +62,14 @@ async def get_pageshot(message: Message, bot) -> None:
             str(message.date)
         )
 
-        # pageshot = open(path_pageshot, 'rb')
-        # await bot.send_photo(chat_id, pageshot, caption="SEND_PAGESHOT")
+        pageshot = open(path_pageshot, 'rb')
+        await bot.send_photo(chat_id, pageshot, caption="SEND_PAGESHOT")
         # task = create_pageshot.s(message.text, message.chat.id,
         #                          message.from_user.id, message.date)
         # callback = handlers_bot.s(bot)
         # return chord(task)(callback)
         # open("PATH_PAGESHOT".format(), 'rb')
-        # await bot.send_photo(c.message.chat.id, Photo_lsd, caption='Я работаю')
+        # await bot.send_photo(
+        # c.message.chat.id, Photo_lsd, caption='Я работаю')
     else:
         await message.answer(msg.ERROR_URL)
-
-
-# @router.message()
-# async def update_message(message: Message, bot) -> None:
-#     """Обработчик обновление сообщения, добовление pageshot."""
-#     if validators.url(message.text):
-#         await message.answer(msg.SEND_REQUEST)
-#         create_pageshot
-#         pageshot(message.text, message.chat.id, message.date)
-#         open("PATH_PAGESHOT".format(), 'rb')
-#         await bot.send_photo(c.message.chat.id, Photo_lsd, caption='Я работаю')
-#     else:
-#         await message.answer(msg.ERROR_URL)
-
-
-
-
-
-# @router.message(Command(cmd.MAIN))
-# @router.callback_query(F.data == cmd.MAIN)
-# async def cmd_menu(
-#     call_or_message: CallbackQuery | Message,
-#     state: FSMContext,
-# ) -> None:
-#     """Обработчик вызова главного меню."""
-#     await state.clear()
-#     if isinstance(call_or_message, Message):
-#         await call_or_message.answer(msg.MAIN_MSG, reply_markup=main_kb())
-#     else:
-#         await call_or_message.message.edit_text(
-#             msg.MAIN_MSG, reply_markup=main_kb()
-#         )
-
-
-# @router.message(Command(cmd.CANCEL))
-# @router.message(F.text == btn.CANCEL_TXT)
-# @router.callback_query(F.data == cmd.CANCEL)
-# async def cmd_cancel(
-#     call_or_message: CallbackQuery | Message,
-#     state: FSMContext,
-# ) -> None:
-#     """Обработчик команды отмены."""
-#     if await state.get_state():
-#         await state.clear()
-#         if isinstance(call_or_message, Message):
-#             await call_or_message.answer(
-#                 msg.CANCEL_MSG, reply_markup=ReplyKeyboardRemove()
-#             )
-#         else:
-#             await call_or_message.message.edit_text(msg.CANCEL_MSG)
-#     else:
-#         await call_or_message.answer(
-#             msg.STATE_CLEAR, reply_markup=ReplyKeyboardRemove()
-#         )
