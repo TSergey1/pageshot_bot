2fb83e2006579ae44d7e948ff9de3df9054266e1
Сергей Тыртычный <tyrtychnyy90@mail.ru>
2024-08-25 16:12:20 +0300

    Ok

--- app/config.py
+++ app/config.py
@@ -15,7 +15,6 @@ GROUP = os.getenv("GROUP_ID")
 
 # DB
 DB_TYPE = os.getenv("DB_TYPE")
-DB_CONNECTOR = os.getenv("DB_CONNECTOR")
 DB_HOST = "localhost" if DEBUG else os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT", 5432)
 POSTGRES_DB = os.getenv("POSTGRES_DB", "bot")
